ebfbd7c2ae32c2ea8b25554237476fcd5448cfae
Бекбаев Ийстэел <izmailov282002@list.ru>
2020-09-19 17:12:24 +0300

    first

---
+++ Practice/01/C++/PRT1/PRT1/Source1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main()
+{
+std: : cout << "Результат выражения 2 + 2 * 2 = ";
+}
---
+++ Practice/02/C++/Source2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+	int x = 3;
+	int y = 3.14;
+
+	double x
+		double y
+		std : : cout << x / n;
+std: : cout << y
+
+
+
+
+}
+
