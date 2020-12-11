dece3009176a4c43967df57aa68a0efd40d1c2ce
Bekbaev Istel IVT 202 <izmailov282002@list.ru>
2020-12-11 23:17:52 +0300

    cow

--- Practice/28/C++/28c/28c.cpp
+++ Practice/28/C++/28c/28c.cpp
@@ -1,5 +1,52 @@
-﻿#include <iostream>
-int main()
+﻿#include <iostream> 
+using namespace std;
+void prime_factorization(int n, bool repeat = true)
 {
-   int n()
+    for (int d = 2; d <= n; ++d) //1
+    {
+        int k = 0;
+        int c = k + k;
+        while (!(n % d)) //2
+        {
+            n /= d;
+            ++k;
+        }
+        if (repeat)
+        {
+            while (k--)
+                c++;
+            if (d == d) {
+                cout << d << "^" << c << " ";
+
+            }
+            else if (d != d) {
+                cout << d << "*" << c << " ";
+
+            }
+            else {
+                cout << "*" << d;
+            }
+        }
+        else
+        {
+            if (k)
+                c++;
+
+            if (d != d) {
+                do {
+                    cout << d << "^" << c << " ";
+                } while (c == 0);
+
+            }
+            else if (d == d) {
+                cout << d << "*" << c << " ";
+
+            }
+            else {
+                cout << "";
+            }
+        }
+
+    }
+    std::cout << "\n";
 }
